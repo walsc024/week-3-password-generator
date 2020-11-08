365c72551fe5c287231d0671e79d80f6e5210f85
christopher walsh <chriswalsh08@googlemail.com>
2020-11-08 11:31:54 +0000

    Checks added for the length of password

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  // Confirming with the user which characters they want to use
   var upperCase = confirm('Do you want Upper Case Letters?');
   var lowerCase = confirm('Do you want Lower Case?'); 
   var specialChar = confirm("Do you want special characters?");
   var numbers = confirm('Do you want numbers?');
-
+  //Check to make sure they have selected something and alerting and returning a string if not
   if (upperCase === false && lowerCase === false && specialChar === false && numbers === false){
    alert('Please select at least one type of character');
    return ""; 
   }
 
+  //Confirming what length of password the user wants
+  var lengthPasswordString = prompt('What length do you want the password to be?')
+  var lengthPasswordNumber = parseInt(lengthPasswordString); 
+  // Check that the requested length meets the length requirements
+  if (lengthPasswordNumber < 8 || lengthPasswordNumber > 128){
+    alert('Please select a number greater than 7 but less than 129');
+    return ""; 
+  }
  
 }
 
